b4380c15b682a0d42ad4a5554f7d6f622126daa4
Akhil Nair <Akhil.Nair@einfochips.com>
2023-02-07 12:18:47 +0530

    initial commit

---
+++ score.js
@@ -0,0 +1,12 @@
+function myfun(){
+    var myname1 = document.getElementById("uname1").value;
+    var myname2 = document.getElementById("uname2").value;
+
+    if(myname1 == myname2){
+    alert("Both are equal");
+    }
+    else
+    {
+        alert("Both are not equal");
+    }
+}
