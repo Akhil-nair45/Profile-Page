70a4ed11030a719de8fc0f4a56d70125fb85e4cf
Akhil Nair <Akhil.Nair@einfochips.com>
2023-02-20 09:51:56 +0530

    Activity for day10

--- Exceptionss/ExceptionExample1.java
+++
@@ -1,7 +0,0 @@
-public class ExceptionExample1 {
-    public static void main(String[] args) {
-        System.out.println("First Line");
-        System.out.println("Result: " +3/0);
-        System.out.println("Last Line");
-    }
-}
